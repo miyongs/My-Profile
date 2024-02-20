f67bfdf952ceac964e5f77dd4da144aac5a873d8
Amhir P <amhirpizarra2001@gmail.com>
2024-02-19 18:44:56 -0800

    link css

--- home.php
+++ home.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Myong Dev</title>
+
+    <link rel="stylesheet" href="../home_style.css">
 </head>
 <body>
 
-<h1>Hello! Welcome to Myong Dev's Profile</h1>
-    
+<h1>Hello! Welcome to Myong Dev's Profile</h1>  
+
+
 </body>
 </html>
